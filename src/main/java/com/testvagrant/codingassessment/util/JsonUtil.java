f74d0673132c9b949c8822fdf7e21eedd39f95ee
jasr9001 <srimathi.janakiraman@nielsen.com>
2022-11-07 12:20:38 +0530

    [SELF] - .gitignore changes and JsonUtil cleanup

--- src/main/java/com/testvagrant/codingassessment/util/JsonUtil.java
+++ src/main/java/com/testvagrant/codingassessment/util/JsonUtil.java
@@ -1,7 +1,5 @@
 package com.testvagrant.codingassessment.util;
 
-import com.testvagrant.codingassessment.constants.FrameworkConstants;
-import netscape.javascript.JSObject;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -10,9 +8,6 @@ import org.json.simple.parser.ParseException;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 public class JsonUtil {
 
@@ -43,22 +38,4 @@ public class JsonUtil {
         return jsonObject.toJSONString();
     }
 
-    public static void main(String[] args) {
-        try {
-            JSONParser parser = new JSONParser();
-
-            JSONObject jsonObject = (JSONObject) parser.parse(
-                    new FileReader("C:/TestVagrantCodingAssessment/src/test/resources/TeamRCB.json"));//path to the JSON file.
-
-                     JSONParser jsonParser = new JSONParser();
-          //Object object = jsonParser.parse(j);
-
-            System.out.println(JsonUtil.getJsonAsString(jsonObject));
-
-
-
-        } catch (IOException | ParseException e) {
-            e.printStackTrace();
-        }
-    }
 }
