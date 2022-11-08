cb0551134ba27f10e3097e0b4f58601217c1da64
jasr9001 <srimathi.janakiraman@nielsen.com>
2022-11-08 15:17:28 +0530

    [SELF] - RCB Team Members validation - Solution 2

--- src/test/java/RCBTeamTest.java
+++ src/test/java/RCBTeamTest.java
@@ -1,22 +1,23 @@
 import com.testvagrant.codingassessment.constants.FrameworkConstants;
 import com.testvagrant.codingassessment.util.JsonUtil;
 import org.testng.Assert;
+import com.jayway.jsonpath.*;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
 public class RCBTeamTest {
-    String jsonResponseString = JsonUtil.getJsonAsString(JsonUtil.getJsonFileContent(FrameworkConstants.JSON_FILE_PATH,"TeamRCB.json"));
+    String jsonResponseString = JsonUtil.getJsonAsString(JsonUtil.getJsonFileContent(FrameworkConstants.JSON_FILE_PATH,FrameworkConstants.JSON_FILE_NAME));
 
     @Test
     public void validateForeignPlayersCount() {
-        List<String> foreignPlayersCountry = com.jayway.jsonpath.JsonPath.read(jsonResponseString,"$..player[?(@.country!='India')]");
+        List<String> foreignPlayersCountry = JsonPath.read(jsonResponseString,"$..player[?(@.country!='India')]");
         Assert.assertEquals(4,foreignPlayersCountry.size(),"Foreign players count should be matched");
     }
 
     @Test
     public void validateWicketKeepersPresence() {
-        List<String> wicketKeepers = com.jayway.jsonpath.JsonPath.read(jsonResponseString,"$..player[?(@.role=='Wicket-keeper')]");
+        List<String> wicketKeepers = JsonPath.read(jsonResponseString,"$..player[?(@.role=='Wicket-keeper')]");
         Assert.assertNotNull(wicketKeepers.size(),"Team should contain at least 1 Wicket-keeper");
     }
 }
---
+++ src/test/java/RCBTeamTest_RestAssured.java
@@ -0,0 +1,42 @@
+import com.testvagrant.codingassessment.constants.FrameworkConstants;
+import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class RCBTeamTest_RestAssured {
+    String jsonBody;
+    JsonPath jsonPath;
+
+    @BeforeClass
+    public JsonPath readJsonFile() {
+        byte[] jsonResponse = new byte[0];
+        try {
+            jsonResponse = Files.readAllBytes(Paths.get(FrameworkConstants.JSON_FILE_PATH+FrameworkConstants.JSON_FILE_NAME));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        jsonBody = new String(jsonResponse);
+
+        return jsonPath=new JsonPath(jsonBody);
+    }
+
+    @Test
+    public void validateForeignPlayersList() {
+       System.out.println(readJsonFile().getList("player.findAll{it.country !='India'}"));
+       Assert.assertEquals(readJsonFile().getList("player.findAll{it.country !='India'}").size(),4,"Foreign players count should be matched");
+
+    }
+
+    @Test
+    public void validatePresenceOfWicketKeeper() {
+        System.out.println(readJsonFile().getList("player.findAll{it.role=='Wicket-keeper'}"));
+        Assert.assertTrue(readJsonFile().getList("player.findAll{it.role=='Wicket-keeper'}").size()>0,"RCB team should contain at least 1 wicket-keeper");
+
+    }
+}
